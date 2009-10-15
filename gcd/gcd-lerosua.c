8b05836f3c62c5046d4ceed76a9e2ad89d3eaa3f
lerosua <lerosua@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2009-10-15 14:18:49 +0000

    init

---
+++ gcd/gcd-lerosua.c
@@ -0,0 +1,56 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  gcd.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2009年10月15日 20时34分49秒 CST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  lerosua (), lerosua@gmail.com
+ *        Company:  Cyclone
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int mod(int a,int b)
+{
+	return a%b;
+}
+
+int gcd(int a,int b)
+{
+	int c=0;
+	do{
+		c=mod(a,b);
+		//printf("%d %% %d =%d\n",a,b,c);
+		if(c==0)
+			return b;
+		a=b;
+		b=c;
+	}while(c>0);
+
+	//return c;
+
+
+}
+
+int main(int argc,char *argv[])
+{
+ 
+	int a=28;
+	int b=14;
+	printf("输入两个整数: ");
+	scanf("%d %d",&a,&b);
+	printf("\n");
+
+	printf("最大公约数 gcd(%d,%d) = %d\n",a,b,gcd(a,b));
+
+	    return 0;
+}
+
