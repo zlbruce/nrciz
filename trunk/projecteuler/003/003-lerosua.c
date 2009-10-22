0308581ef9da34477f7d55e90e2bfdd1012abcf6
lerosua <lerosua@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2009-10-22 00:49:27 +0000

    fix 003-lerosua.c

--- trunk/projecteuler/003/003-lerosua.c
+++ trunk/projecteuler/003/003-lerosua.c
@@ -18,8 +18,11 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 #define MAX 1024
+//#define DEBUG
 /** 简单的素数集合*/
 static int primum[MAX] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
 
@@ -34,8 +37,12 @@ void fill_prime(int num)
 	int j = 0;
 	for (; i < num; i = i + 2) {
 		int flag = 1;
-		for (j = 0; j < count; j++) {
-			if (i % primum[j] == 0) {
+		int m=sqrt(i)+1;
+		//for (j = 0; j < count; j++) {
+		for (j = 0; primum[j] < m; j++) {
+			
+			//if ((primum[j]>m || i % primum[j] == 0)) {
+			if (( i % primum[j] == 0)) {
 				flag = 0;
 				break;
 			}
@@ -65,6 +72,7 @@ int main(int argc, char *argv[])
 
 #if 1
 	long long sum = 600851475143L;
+	//long long sum = 8462696833;
 	//long long sum=13195;
 
 	int j = 0;
@@ -79,7 +87,7 @@ int main(int argc, char *argv[])
 	}
 
 #ifdef DEBUG
-	printf(" %d 以内素数组为: \n", sum);
+	printf(" %lld 以内素数组为: \n", sum);
 	for (i = 0; i < j; i++)
 		printf("%d ", yin[i]);
 
