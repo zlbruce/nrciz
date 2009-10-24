dc03d9ff5c2b580eb01805a43425cc4a26aa0b47
xioooli <xioooli@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2009-10-24 05:56:50 +0000

    xiooli solved 013 using bash

---
+++ projecteuler/013/013-xiooli.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Name:     013-xiooli.sh
+# Author:   xiooli
+#           Email:  <xioooli[at]yahoo.com.cn>
+#           Site:   http://joolix.com
+# Licence:  GPLv3
+# Version:  091024
+
+bc -l <<<"`cat digs |tr "\n" "+"`"|head -c10
+echo
+
+
