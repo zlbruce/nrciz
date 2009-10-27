4713f5ddca3dd09f826ad57722ca97cfa3db1fa6
guotianxiang@gmail.com <guotianxiang@gmail.com@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2009-10-27 06:03:24 +0000

    nacre 8queen, 没有考虑旋转翻折之类的相同性

---
+++ trunk/8queen/8queen-nacre.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+def num_solution(n):
+    from itertools import permutations
+    num = 0
+    # p[i] = j means that the queen is on i-th row, j-th column
+    for p in permutations(range(n)):
+        if is_solution(p):
+            num += 1
+    return num
+
+def is_solution(p):
+    n = len(p)
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if abs(p[i] - p[j]) == j - i:
+                return 0
+    return 1
+
+print num_solution(8)
