a040967978f9d8a950a267fe1bd5a07c536cb814
ivenvd@gmail.com <ivenvd@gmail.com@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2009-10-27 15:32:10 +0000

    ivenvd 003-005

---
+++ projecteuler/005/005-ivenvd.c
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  005-ivenvd.c
+ *
+ *    Description:  What is the smallest number divisible by each of the numbers 1 to 20?
+ *
+ *        Version:  1.0
+ *        Created:  2009年10月27日 17时05分13秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Xu Lijian (ivenvd), ivenvd@gmail.com
+ *        Company:  CUGB, China
+ *
+ * =====================================================================================
+ */
+
+#include	<stdio.h> 
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  main function
+ * =====================================================================================
+ */
+	int
+main (int argc, char *argv[])
+{
+    printf ("Result: %d.\n", 16 * 9 * 5 * 7 * 11 * 13 * 17 * 19);
+
+	return 0;
+}		/* ----------  end of function main  ---------- */
+
