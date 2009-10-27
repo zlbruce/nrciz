f61f8509e1f6ec691a14c15df13ef62ff03da877
ivenvd@gmail.com <ivenvd@gmail.com@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2009-10-27 03:23:27 +0000

    ivenvd 003

---
+++ trunk/projecteuler/003/003-ivenvd.c
@@ -0,0 +1,77 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  003-ivenvd.c
+ *
+ *    Description:  Find the largest prime factor of a composite number.
+ *
+ *        Version:  1.0
+ *        Created:  2009年10月27日 10时29分05秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Xu Lijian (ivenvd), ivenvd@gmail.com
+ *        Company:  CUGB, China
+ *
+ * =====================================================================================
+ */
+
+#include	<stdio.h> 
+#include	<math.h>
+
+#define	NUM 600851475143			/*  */
+
+    static int
+check_prime (long num)
+{
+    int i;
+    static char flag = 0;                           /* 嵌套标志 */
+    if (flag && (num % 5 == 0 || num % 2 == 0)) {
+        flag = 0;
+        return 1;
+    }
+    for (i = sqrt (num); i > 1; i--) {
+        switch (i % 10) {
+            case 1:	
+            case 3:	
+            case 7:	
+            case 9:	
+                if (num % i == 0) {
+                    if (flag) {
+                        flag = 0;
+                        return 0;               /* 因子为和数 */
+                    } else {
+                        flag = 1;
+                        if (check_prime (i)) {
+                            return i;
+                        }
+                    }
+                }
+                break;
+            default:	
+                break;
+        }				/* -----  end switch  ----- */
+    }
+    flag = 0;
+    return 1;
+}		/* -----  end of static function check_prime  ----- */
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  main function
+ * =====================================================================================
+ */
+	int
+main (int argc, char *argv[])
+{
+    int factor = check_prime (NUM);
+    if (factor == 1) {
+        printf ("The number %ld is prime.\n", NUM, factor);
+    } else {
+        printf ("The largest prime factor of %ld is %d.\n", NUM, factor);
+    }
+
+	return 0;
+}		/* ----------  end of function main  ---------- */
+
