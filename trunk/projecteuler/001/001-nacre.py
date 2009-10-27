6080b0e61990c692fd0bf002026b2063ce9c2298
guotianxiang@gmail.com <guotianxiang@gmail.com@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2009-10-27 05:44:37 +0000

    nacre 1

---
+++ trunk/projecteuler/001/001-nacre.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+def sum_multiple(feed, limit):
+    return feed * ((limit - 1) / feed) * ((limit - 1) / feed + 1) / 2
+
+sum = sum_multiple(3, 1000) + sum_multiple(5, 1000) - sum_multiple(3 * 5, 1000)
+print sum
