040d10793b1dadae2591f53b4c5319e45d9e2473
guotianxiang@gmail.com <guotianxiang@gmail.com@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2009-10-27 04:24:18 +0000

    nacre 3-5

---
+++ projecteuler/004/004-nacre.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+maxnum = 0
+for i in range(990, 99, -11):
+    for j in range(999, i, -1):
+	num = i * j
+    	if num <= maxnum:
+	    break
+	else:
+	    strnum = str(num)
+	    if strnum == strnum[::-1]:
+		maxnum = num
+		break
+print maxnum
