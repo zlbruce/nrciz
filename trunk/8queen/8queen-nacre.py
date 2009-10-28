83d48d1bdea9b47dedafbcc6d5ab77e88c3915fa
guotianxiang@gmail.com <guotianxiang@gmail.com@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2009-10-28 05:44:04 +0000

    get number of unique solutions

--- trunk/8queen/8queen-nacre.py
+++ trunk/8queen/8queen-nacre.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python
 
-def num_solution(n):
+def get_solution(n):
     from itertools import permutations
-    num = 0
-    # p[i] = j means that the queen is on i-th row, j-th column
+    vp, up = [], []
+    # p[i] = j means that the queen is on i-th column, j-th row
     for p in permutations(range(n)):
         if is_solution(p):
-            num += 1
-    return num
+            vp.append(list(p))
+    cp = vp[:]
+    while len(cp) != 0:
+        p = cp[0]
+        up.append(p)
+        for mp in (p, t(p), t(t(p)), t(t(t(p))), r(p), t(r(p)), t(t(r(p))), t(t(t(r(p))))):
+            try:
+                cp.remove(mp)
+            except ValueError:
+                pass
+    return vp, up
 
 def is_solution(p):
     n = len(p)
@@ -17,4 +26,24 @@ def is_solution(p):
                 return 0
     return 1
 
-print num_solution(8)
+# transpose
+def t(p):
+    n = len(p)
+    tp = [0] * n
+    for i in range(n):
+        tp[p[i]] = n - i - 1
+    return tp
+
+# reflect
+def r(p):
+    n = len(p)
+    rp = [0] * n
+    for i in range(n):
+        rp[p[i]] = i
+    return rp
+
+n = 8
+vp, up = get_solution(n)
+print n, "queens problem"
+print "number of valid solution ", len(vp)
+print "number of unique solution", len(up)
