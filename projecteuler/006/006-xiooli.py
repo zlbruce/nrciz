156ab915e181dbb7d22eaac1c6d03f6c54dc80ac
xioooli <xioooli@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2009-10-29 07:23:00 +0000

    xiooli solved 006 in python

---
+++ projecteuler/006/006-xiooli.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Name:     006-xiooli.py
+# Author:   xiooli
+#           Email:  <xioooli[at]yahoo.com.cn>
+#           Site:   http://joolix.com
+# Licence:  GPLv3
+# Version:  091024
+
+import math as m
+print int(m.pow(sum([i for i in range(1,101)]),2)) - sum([i*i for i in range(1,101)])
+
