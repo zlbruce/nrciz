93c13b9a6196d82917db4f060c8a4285c6d7e00e
guotianxiang@gmail.com <guotianxiang@gmail.com@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2009-10-29 12:24:06 +0000

    improve 048

--- trunk/projecteuler/048/048-nacre.py
+++ trunk/projecteuler/048/048-nacre.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 
-print sum(i**i for i in range(1001)) % 10 ** 10
+nd = 10
+x = 10 ** nd
+
+def sum10(base, power):
+    if power == 1:
+        return base
+    elif power % 2 == 0:
+        return (sum10(base, power / 2) ** 2) % x
+    else:
+        return (sum10(base, power / 2) ** 2 * base) % x
+
+print sum(sum10(i, i) for i in range(1, 1001)) % x
