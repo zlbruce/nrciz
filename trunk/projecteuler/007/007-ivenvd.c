32300598f543835b842d445617d0a7de34eba3da
ivenvd@gmail.com <ivenvd@gmail.com@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2009-10-29 06:58:06 +0000

    ivenvd 007, 009

--- trunk/projecteuler/007/007-ivenvd.c
+++ trunk/projecteuler/007/007-ivenvd.c
@@ -30,25 +30,28 @@
 	int
 main (int argc, char *argv[])
 {
+    char is_prime;
     int i, n = 5, cnt = 3;
     int prime[COUNT] = {2, 3, 5};               /* 质数库 */
     while (n += 2) {                            /* 从 7 开始验证 */
         if (n % 5 == 0) {
             continue;
         }
-        for (i = 0; i < cnt; i++) {
+        is_prime = 1;
+        for (i = 0; prime[i] <= sqrt(n); i++) {
             if (n % prime[i] == 0) {
+                is_prime = 0;
                 break;
             }
         }
-        if (i == cnt) {                         /* 是质数 */
+        if (is_prime) {                         /* 是质数 */
             prime[cnt++] = n;                   /* 存入质数库 */
             if (cnt == COUNT) {
                 break;
             }
         }
     }
-    printf ("The 10001st prime number is %d.\n", n);
+    printf ("%d\n", n);
 
 	return 0;
 }		/* ----------  end of function main  ---------- */
