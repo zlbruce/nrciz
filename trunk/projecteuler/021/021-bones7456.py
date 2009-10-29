eb96eb2da95862dc66c3f3321f32794b1c5a365b
bones7456 <bones7456@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2009-10-29 04:16:37 +0000

    euler 023 /modify 012

--- trunk/projecteuler/021/021-bones7456.py
+++ trunk/projecteuler/021/021-bones7456.py
@@ -4,11 +4,11 @@ from math import sqrt
 def d(n):
     '''return sum of proper divisors of n'''
     r=1
-    for i in range (2, int(sqrt(n)) ):
+    for i in range (2, int(sqrt(n))+1 ):
         if 0 == n % i:
             r += i + n /i
     if sqrt(n) == int(sqrt(n)):
-        r += int(sqrt(n))
+        r -= int(sqrt(n))
     return r
     
 print sum([i for i in range(3,10000) if i!=d(i) and i==d(d(i))])
