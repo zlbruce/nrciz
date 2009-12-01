91d86d635e2ad11999da641a06451d9bd56579ff
hohoxl@gmail.com <hohoxl@gmail.com@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2009-12-01 01:58:30 +0000

    add 005-rem1x.py

---
+++ trunk/projecteuler/005/005-rem1x.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+'''
+copy from narce
+'''
+
+def gcd(m, n):
+    if m < n:
+	m, n = n, m
+    while m % n != 0:
+	    m, n = n, m % n
+    return n
+
+def lcm(m, n):
+    return m * n / gcd(m, n)
+
+print reduce(lcm, range(1, 21))
