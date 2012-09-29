75da45c376667b7a1fe4deb59d3621b8a8f45114
zhangleibruce@gmail.com <zhangleibruce@gmail.com@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2012-09-29 09:00:59 +0000

    不需要IntSet

--- projecteuler/035/035-zlbruce.hs
+++ projecteuler/035/035-zlbruce.hs
@@ -1,6 +1,5 @@
 import Data.List
 import Data.Numbers.Primes
-import qualified Data.IntSet as S
 
 rotations n = foldl genR [n] [1..nLen - 1]
     where 
@@ -9,13 +8,8 @@ rotations n = foldl genR [n] [1..nLen - 1]
           where n'     = m * (10 ^ (nLen - num)) + d
                 (d, m) = n `divMod` (10 ^ num)
 
-allPrime = S.fromList $ takeWhile (<1000000) primes
+allPrime = takeWhile (<1000000) primes
 
-isOk = and . map isInPrime . rotations
+isOk = and . map isPrime . rotations
 
--- 必然是由1,3,5,7组成的数
-isInPrime n
-    | even n && n > 2 = False
-    | otherwise       = S.member n allPrime
-
-main = print $ S.size $ S.filter isOk allPrime
+main = print $ length $ filter isOk allPrime
--- projecteuler/037/037-zlbruce.hs
+++ projecteuler/037/037-zlbruce.hs
@@ -1,6 +1,5 @@
 import Data.List
 import Data.Numbers.Primes
-import qualified Data.IntSet as S
 
 truncs n = nub $ foldl genT [n] [1..nLen - 1]
     where 
@@ -9,13 +8,6 @@ truncs n = nub $ foldl genT [n] [1..nLen - 1]
           where l' = n `div` 10 ^ (nLen - num)
                 r' = n `mod` 10 ^ num
 
--- 这里假定了这些数在1000000以内
-allPrime = S.fromList $ takeWhile (<1000000) primes
-
-isOk = and . map isInPrime . truncs
-
-isInPrime n
-    | even n && n > 2 = False
-    | otherwise       = S.member n allPrime
+isOk = and . map isPrime . truncs
 
 main = print . sum . take 11 . filter isOk $ dropWhile (<10) primes
