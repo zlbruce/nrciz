09c61db9a4a19ec0a98fb2395692ebe5e65f37de
zhangleibruce@gmail.com <zhangleibruce@gmail.com@7ad355ac-b993-11de-8dfb-43c0dffa5306>
2012-09-29 09:38:32 +0000

    038

---
+++ projecteuler/038/038-zlbruce.hs
@@ -0,0 +1,18 @@
+import Data.List
+-- n > 1,  num < 10000
+--
+
+conProd n = genConProd "" n 1
+genConProd res n t = 
+    if resLen > 9
+    then 0
+    else if resLen == 9
+    then if nubResLen == 9 then read res else 0
+    else genConProd res' n t'
+    where
+        resLen    = length res
+        nubResLen = length $ nub res
+        res'      = res ++ show (n * t)
+        t'        = t + 1
+
+main = print $ maximum $ map conProd [1..9999]
